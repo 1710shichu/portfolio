7b73b324ea239a78e9cf55561b7e20c7db779401
1710shichu <157471262+1710shichu@users.noreply.github.com>
2024-05-22 13:31:30 +0900

    2

---
+++ Emerald/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    // カルーセル
+    $('.carousel').slick({
+      autoplay: true,
+      dots: true,
+      infinite: true,
+      autoplaySpeed: 3000,
+      arrows: false,
+    });
+  });
